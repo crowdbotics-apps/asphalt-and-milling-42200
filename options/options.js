226aef325a325ffcc39446aa3f497e9de5fe68b5
Crowdbotics <team@crowdbotics.com>
2023-07-14 22:32:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "asphalt-and-milling-42200",
+  url: "https://asphalt-and-milling-42200.botics.co",
+  api: "https://asphalt-and-milling-42200.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
